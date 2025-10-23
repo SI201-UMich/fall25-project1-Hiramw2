f4ad93dad1d19d76e4cf893eaa2185412f730911
Hiram <hiramwillis2@gmail.com>
2025-10-23 12:08:32 -0400

    had too many files deleted some of them

--- tests/conftest.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-# Ensure repo root is importable when running pytest from workspace
-ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-if ROOT not in sys.path:
-    sys.path.insert(0, ROOT)
